dff1bcdd52a6a7eaebcb4840648fc968e4ff896d
Minji Lee <mingg9688@gmail.com>
2020-01-17 15:58:02 +0900

    add load&test mode, divide training.py file

--- main.py
+++ main.py
@@ -1,181 +1,43 @@
+import os
 import numpy as np
-from scipy.io import loadmat
 import numpy.linalg as LA
 from math import sqrt, floor
-import matplotlib.pyplot as plt
 
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
-from tensorflow import keras
-from tensorflow.keras.models import Sequential, model_from_json
-from tensorflow.keras.layers import Dense
 
 from module.normalize import Normalize
+from module.training import Train
 from datetime import datetime
 
 
-def dataLoader(filename, input, output):
-    mat_contents = loadmat(filename)
-    i_data = mat_contents[input]
-    o_data = mat_contents[output]
-    return i_data, o_data
-
-
-def trainValidTestSplit(i_data, o_data, tr, va, te):
-    train_i = []
-    train_o = []
-    test_i = []
-    test_o = []
-    valid_i = []
-    valid_o = []
-
-    for i in range(len(o_data)):
-        if i % 100 < int(tr * 100) :
-            train_i.append(i_data[i,:])
-            train_o.append(o_data[i])
-        elif i % 100 < int((tr + va) * 100):
-            valid_i.append(i_data[i, :])
-            valid_o.append(o_data[i, :])
-        else :
-            test_i.append(i_data[i, :])
-            test_o.append(o_data[i, :])
-
-    train_i = np.array(train_i)
-    train_o = np.array(train_o)
-    test_i = np.array(test_i)
-    test_o = np.array(test_o)
-    valid_i = np.array(valid_i)
-    valid_o = np.array(valid_o)
-    return train_i, train_o, valid_i, valid_o, test_i, test_o
-
-class Train:
-    def __init__(self, err, m_file_name = "", w_file_name = ""):
-        self.error_bound = err
-        if (m_file_name == "" and w_file_name == ""):
-            self.model = Sequential()
-            # training
-            self.model.add(Dense(256, input_dim = 3, activation = 'relu'))
-            self.model.add(Dense(256, activation = 'relu'))
-            self.model.add(Dense(256, activation = 'relu'))
-            self.model.add(Dense(1, activation = 'tanh'))
-
-        else:
-            self.model = self.loadFile(m_file_name, w_file_name)
-
-    def train(self, i_data, o_data, save):
-        print(self.model)
-        train_i, train_o, valid_i ,valid_o, test_i, test_o = \
-            trainValidTestSplit(i_data, o_data, 0.7, 0.15, 0.15)
-        # scaler = StandardScaler()
-        # scaler.fit(train_o)
-        # train_input = scaler.transform(train_i)
-
-        n = Normalize(train_i, train_o)
-        train_input, train_output = n.dataNormalize(train_i, train_o)
-        valid_input, valid_output = n.dataNormalize(valid_i, valid_o)
-
-        self.model.compile(loss = 'mean_squared_error', optimizer = 'adam',
-            metric = ['accuracy'])
-        self.model.fit(train_input, train_output, epochs = 30,
-            batch_size = 10,
-            validation_data = (valid_input, valid_output)
+def training(
+    error_bound,
+    file_path,
+    figure_save_directory,
+    model_save_directory,
+    file_name,
+    data_num,
+    new = True,
+    m_file_name = "",
+    w_file_name = ""
+    ):
+    if new:
+        t = Train(
+            error_bound,
+            save_directory = figure_save_directory
             )
-        
-        test_input, test_output = n.dataNormalize(test_i, test_o)
-
-        min, max, left_i, right_i = self.evaluation(test_input, test_output, n)
-        return min, max, left_i ,right_i
-
-    def loadFile(self, m_file_name, w_file_name):
-        # load model
-        json_file = open(m_file_name, 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        loaded_model = model_from_json(loaded_model_json)
-        # load weight
-        loaded_model.load_weights(w_file_name)
-        print("Loaded model from disk, name: " + m_file_name)
-        return loaded_model
-    
-    def saveFile(self, m_file_name, w_file_name):
-        # serialize model to JSON
-        model_json = self.model.to_json()
-        with open(m_file_name, "w") as json_file:
-            json_file.write(model_json)
-
-        # serialize weights to HDF5
-        self.model.save_weights(w_file_name)
-        print("Save model to disk, name: " + m_file_name)
-
-    def evaluation(self, i_data, o_data, n):
-        # predict
-        prediction = self.model.predict(i_data)
-        predict_output = n.dataUnnormalize(prediction)
-        real_output = n.dataUnnormalize(o_data)
-
-        sorted_ind = sorted(range(len(real_output)), key = lambda k:real_output[k])
-        sorted_prediction = np.array([predict_output[i] for i in sorted_ind])
-        sorted_output = np.array([real_output[i] for i in sorted_ind])
-
-        error = abs(sorted_prediction - sorted_output)
-
-        print("max error: %.10f" %(np.max(error)))
-
-        x = range(len(sorted_output))
-        plt.plot(x, sorted_prediction, 'b', sorted_output, 'r')
-        plt.savefig('figure/figure_'+str(datetime.now())+'.png')
-        plt.clf()
-        # plt.show()
-
-        for idx in range(len(sorted_output)):
-            if sorted_output[idx] > -0.004:
-                break
-        left_i = 0
-        for i in range(idx, -1, -1):
-            if error[i] > self.error_bound:
-                left_i = i
-                break
-        print("left i: %d" %left_i)
-        right_i = len(sorted_output) - 1
-        for j in range(idx, len(sorted_output)):
-            if error[j] > self.error_bound:
-                right_i = j
-                break
-        print("right i: %d" %right_i)
-
-        print("bound: {} ~ {}".format(sorted_output[left_i], sorted_output[right_i]))
-        return sorted_output[left_i], sorted_output[right_i], left_i, right_i
-
-    def loadAndTest(self, file_path):
-        filename = 'obj/data/data_final.mat'
-        i_data, o_data = dataLoader(file_path + filename, input, output)
-        train_i, train_o, test_i, test_o = trainTestSplit(i_data, o_data)
-        n = Normalize(test_i, test_o)
-        test_input, test_output = n.dataNormalize(test_i, test_o)
-
-        self.model = loadFile(file_path + 'model.json', file_path + 'model.h5')
-        self.model.compile(loss = 'mean_squared_error', optimizer = 'adam',
-            metric = ['accuracy'])
-        self.evaluation(test_input, test_output, n)
-
-if __name__ == "__main__":
-    data_num = 300000
-    file_path = '/home/mjlee/workspace/NutLearning/'
-    file_name = [
-        'obj/data/data_final1.mat'
-        # 'obj/data/data_final2.mat',
-        # 'obj/data/data_final3.mat',
-        # 'obj/data/data_final4.mat',
-        # 'obj/data/data_final5.mat'
-        ]
-
-    add_ratio = 0.7
-    error_bound = 0.002
-    itr_num = 4
-
-    # loadAndTest(file_path)
-    t = Train(error_bound)
-    i_data, o_data = dataLoader(file_path + file_name[0],
+    else:
+        t = Train(
+            error_bound,
+            m_file_name = m_file_name,
+            w_file_name= = w_file_name,
+            model_save_directory = model_save_directory,
+            save_directory = figure_save_directory,
+            model_
+        )
+    i_data, o_data = t.dataLoader(
+        file_path + file_name[0],
         'input',
         'output') # 1000k
 
@@ -186,19 +48,46 @@ if __name__ == "__main__":
     sorted_input = np.array([i_data[i] for i in sorted_ind])
     sorted_output = np.array(sorted(o_data))
 
-    a, b, left_i, right_i = t.train(sorted_input, sorted_output, False)
-    t.saveFile("model0.json", "model0.h5")
+    real_input = []
+    real_output = []
+    for i in range(len(o_data)):
+        if sorted_output[i] > 0.002:
+            break
+        if sorted_output[i] > -0.002:
+            real_input.append(sorted_input[i])
+            real_output.append(sorted_output[i])
+    sorted_input = np.array(real_input)
+    sorted_output = np.array(real_output)
+    del real_output, real_input
+
+    a, b, left_i, right_i = t.train(
+        sorted_input,
+        sorted_output
+        )
+
+    now = datetime.now()
+    now_string = now.strftime("_%H:%M")
+    model_file = "/model" + now_string + ".json"
+    weight_file = "/model" + now_string + ".h5"
+    t.saveFile(
+        model_save_directory + model_file,
+        model_save_directory + weight_file
+        )
     del t
 
     for itr in range(1, len(file_name)):
-        t = Train(error_bound, "model" + str(itr-1) + ".json",
-            "model" + str(itr-1) + ".h5")
-        new_i, new_o = dataLoader(file_path + file_name[itr],
+        t = Train(
+            error_bound,
+            model_file,
+            weight_file,
+            model_save_directory,
+            figure_save_directory
+            )
+        new_i, new_o = t.dataLoader(
+            file_path + file_name[itr],
             'input',
             'output') # 1000k
 
-        new_i = new_i[1:data_num, :]
-        new_o = new_o[1:data_num, :]
         change_num = 0
         for i in range(len(new_o)):
             if (new_o[i] < a or new_o[i] > b):
@@ -217,6 +106,99 @@ if __name__ == "__main__":
         sorted_input = np.array([sorted_input[i] for i in sorted_ind])
         sorted_output = np.array(sorted(sorted_output))
         
-        a, b, left_i, right_i = t.train(sorted_input, sorted_output, False)
-        t.saveFile('model'+str(itr) +'.json', "model"+str(itr)+".h5")
+        a, b, left_i, right_i = t.train(
+            sorted_input,
+            sorted_output
+            )
+        
+        now = datetime.now()
+        now_string = now.strftime("_%H:%M")
+        model_file = "/model" + now_string + ".json"
+        weight_file = "/model" + now_string + ".h5"
+        t.saveFile(
+            model_save_directory + model_file,
+            model_save_directory + weight_file
+            )
         del t
+
+if __name__ == "__main__":
+    file_path = str(os.getcwd())
+    now = datetime.now()
+    now_string = now.strftime("%Y-%m-%d_%H:%M")
+    now_string = "2020-01-17_14:33"
+    model_save_directory = file_path + '/old_results/' + now_string
+    figure_save_directory = file_path + '/figure/' + now_string
+    try:
+        os.makedirs(figure_save_directory)
+    except:
+        print("already exists")
+
+    ############ LOAD & TEST
+    # m_file = "/old_results/0117:12:00/model6.json"
+    # w_file = "/old_results/0117:12:00/model6.h5"
+    # t = Train(
+    #     m_file_name = file_path + m_file,
+    #     w_file_name = file_path + w_file,
+    #     save_directory = figure_save_directory
+    #     )
+    # t.loadAndTest(file_path)
+
+    ############ TRAINING
+    # try:
+    #     os.makedirs(model_save_directory)
+    # except:
+    #     print("already exists")
+
+    # data_num = 1000000
+    # file_name = [
+    #     '/obj/data/data_final1.mat',
+    #     '/obj/data/data_final1.mat',
+    #     '/obj/data/data_final1.mat'
+    #     # '/obj/data/data_final2.mat',
+    #     # '/obj/data/data_final3.mat',
+    #     # '/obj/data/data_final4.mat',
+    #     # '/obj/data/data_final5.mat'
+    #     ]
+    # error_bound = 0.002
+    # training(
+    #     error_bound,
+    #     file_path,
+    #     figure_save_directory,
+    #     model_save_directory,
+    #     file_name,
+    #     data_num
+    #     )
+    
+    ############ LOAD & TRAIN
+    now_string = "2020-01-17_14:33"
+    model_save_directory = file_path + '/old_results/' + now_string
+    figure_save_directory = file_path + '/figure/' + now_string
+    model_file_name = "/model0.json"
+    weight_file_name = "/model0.h5"
+
+    try:
+        os.makedirs(model_save_directory)
+    except:
+        print("already exists")
+
+    data_num = 1000000
+    file_name = [
+        '/obj/data/data_final1.mat',
+        '/obj/data/data_final1.mat',
+        '/obj/data/data_final1.mat'
+        ]
+    error_bound = 0.002
+    training(
+        error_bound,
+        file_path,
+        figure_save_directory,
+        model_save_directory,
+        file_name,
+        data_num,
+        False,
+        model_file_name,
+        weight_file_name
+        )
+    
+
+
---
+++ module/training.py
@@ -0,0 +1,200 @@
+import sys
+import numpy as np
+from scipy.io import loadmat
+from tensorflow import keras
+from tensorflow.keras.models import Sequential, model_from_json
+from tensorflow.keras.layers import Dense
+
+import matplotlib.pyplot as plt
+
+from module.normalize import Normalize
+from datetime import datetime
+
+class Train:
+    def __init__(self, err = 0.002,
+        m_file_name = "",
+        w_file_name = "",
+        model_save_directory = 'old_results',
+        save_directory = 'figure'
+        ):
+        self.error_bound = err
+        self.figure_save_dir = save_directory
+        self.model_save_dir = model_save_directory
+        if (m_file_name == "" and w_file_name == ""):
+            self.model = Sequential()
+            # training
+            self.model.add(Dense(256, input_dim = 3, activation = 'relu'))
+            self.model.add(Dense(256, activation = 'relu'))
+            self.model.add(Dense(256, activation = 'relu'))
+            self.model.add(Dense(1, activation = 'tanh'))
+
+        else:
+            self.model = self.loadFile(m_file_name, w_file_name)
+
+    def train(self, i_data, o_data):
+        print(self.model)
+        train_i, train_o, valid_i ,valid_o, test_i, test_o = \
+            self.dataSplit(i_data, o_data, 0.7, 0.15, 0.15)
+
+        n = Normalize(train_i, train_o)
+        train_input, train_output = n.dataNormalize(train_i, train_o)
+        valid_input, valid_output = n.dataNormalize(valid_i, valid_o)
+
+        self.model.compile(loss = 'mean_squared_error', optimizer = 'adam',
+            metric = ['accuracy'])
+        self.model.fit(train_input, train_output, epochs = 50,
+            batch_size = 10,
+            validation_data = (valid_input, valid_output)
+            )
+        
+        test_input, test_output = n.dataNormalize(test_i, test_o)
+
+        min, max, left_i, right_i = self.evaluation(
+            test_input,
+            test_output,
+            n)
+        return min, max, left_i ,right_i
+
+    def loadFile(self, m_file_name, w_file_name):
+        # load model
+        try:
+            json_file = open(self.model_save_dir + m_file_name, 'r')
+        except:
+            print("json file failed to load, dir: "
+                + self.model_save_dir + m_file_name)
+            sys.exit(-1)
+
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weight
+        loaded_model.load_weights(self.model_save_dir + w_file_name)
+        print("Loaded model from disk, name: "
+            + self.model_save_dir + m_file_name)
+        return loaded_model
+    
+    def saveFile(self, m_file_name, w_file_name):
+        # serialize model to JSON
+        model_json = self.model.to_json()
+        with open(self.model_save_dir + m_file_name, "w") as json_file:
+            json_file.write(model_json)
+
+        # serialize weights to HDF5
+        self.model.save_weights(self.model_save_dir + w_file_name)
+        print("Save model to disk, name: " + self.model_save_dir
+            + m_file_name)
+
+    def evaluation(self, i_data, o_data, n):
+        # predict
+        prediction = self.model.predict(i_data)
+        predict_output = n.dataUnnormalize(prediction)
+        real_output = n.dataUnnormalize(o_data)
+
+        sorted_ind = sorted(range(len(real_output)), key = lambda k:real_output[k])
+        sorted_prediction = np.array([predict_output[i] for i in sorted_ind])
+        sorted_output = np.array([real_output[i] for i in sorted_ind])
+
+        error = abs(sorted_prediction - sorted_output)
+        
+        print("max error: %.10f" %(np.max(error)))
+
+        x = range(len(sorted_output))
+        plt.plot(x, sorted_prediction, 'b', sorted_output, 'r')
+        now = datetime.now()
+        now_string = now.strftime("%Y-%m-%d_%H:%M")
+        plt.savefig(self.figure_save_dir +'/figure.png')
+        plt.clf()
+        # plt.show()
+
+        for idx in range(len(sorted_output)):
+            if sorted_output[idx] > -0.004:
+                break
+        left_i = 0
+        for i in range(idx, -1, -1):
+            if error[i] > self.error_bound:
+                left_i = i
+                break
+        print("left i: %d" %left_i)
+        right_i = len(sorted_output) - 1
+        for j in range(idx, len(sorted_output)):
+            if error[j] > self.error_bound:
+                right_i = j
+                break
+        print("right i: %d" %right_i)
+        print("bound: {} ~ {}".format(sorted_output[left_i], sorted_output[right_i]))
+
+        right = np.zeros(100).astype(float)
+        wrong= np.zeros(100).astype(float)
+        for i in range(idx, len(sorted_output)):
+            if sorted_output[i] > 0.005:
+                break
+            if sorted_output[i] >= 0:
+                section = int(sorted_output[i] * 20000)
+                if sorted_prediction[i] > 0:
+                    right[section] += 1
+                else:
+                    wrong[section] += 1
+        accuracy = np.zeros(100)
+        for i in range(100):
+            if (wrong[i] + right[i] > 0):
+                accuracy[i] = (right[i] / (wrong[i] + right[i]))
+            else:
+                accuracy[i] = 0
+        X = np.arange(0, 0.005, 0.005 / 100)
+        plt.plot(X, accuracy)
+        # plt.xticks(np.arange(0, 0.005, 0.0002))
+        plt.rc('axes', labelsize = 4)
+        plt.grid()
+        plt.savefig(self.figure_save_dir + '/figure2.png')
+        plt.clf()
+        return sorted_output[left_i], sorted_output[right_i], left_i, right_i
+
+    def loadAndTest(self, file_path):
+        filename = '/obj/data/data_final1.mat'
+        i_data, o_data = self.dataLoader(
+            file_path + filename,
+            'input',
+            'output'
+            )
+        _, _, _, _, test_i, test_o = self.dataSplit(i_data, o_data)
+        n = Normalize(test_i, test_o)
+        test_input, test_output = n.dataNormalize(test_i, test_o)
+        
+        self.model.compile(loss = 'mean_squared_error', optimizer = 'adam',
+            metric = ['accuracy'])
+        self.evaluation(test_input, test_output, n)
+
+    
+    def dataLoader(self, filename, input, output):
+        mat_contents = loadmat(filename)
+        i_data = mat_contents[input]
+        o_data = mat_contents[output]
+        return i_data, o_data
+
+
+    def dataSplit(self, i_data, o_data, tr = 0.7, va = 0.15, te = 0.15):
+        train_i = []
+        train_o = []
+        test_i = []
+        test_o = []
+        valid_i = []
+        valid_o = []
+
+        for i in range(len(o_data)):
+            if i % 100 < int(tr * 100) :
+                train_i.append(i_data[i,:])
+                train_o.append(o_data[i])
+            elif i % 100 < int((tr + va) * 100):
+                valid_i.append(i_data[i, :])
+                valid_o.append(o_data[i, :])
+            else :
+                test_i.append(i_data[i, :])
+                test_o.append(o_data[i, :])
+
+        train_i = np.array(train_i)
+        train_o = np.array(train_o)
+        test_i = np.array(test_i)
+        test_o = np.array(test_o)
+        valid_i = np.array(valid_i)
+        valid_o = np.array(valid_o)
+        return train_i, train_o, valid_i, valid_o, test_i, test_o
