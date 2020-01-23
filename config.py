b87b63a39ec89fdaba204ffcd5e0dbb4a1eaa323
JM <yjm5181@snu.ac.kr>
2020-01-23 13:28:28 +0900

    calculate optimal batch size

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import numpy as np
 
 config = {}
 config["data_num"] = 1000000
-config["epoch"] = 100
-config["batch"] = 10
+config["epoch"] = 20
+config["batch"] = 40
 config["error_bound"] = 0.0005
+config["weight"] = [1, 0.1]
