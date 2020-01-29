5e335d0fdacffc630bbf575e3d8ab6199541604f
Minji Lee <mingg9688@gmail.com>
2020-01-29 22:56:06 +0900

    mend

---
+++ module/data_processing.py
@@ -0,0 +1,62 @@
+import numpy as np
+from module.normalize import Normalize
+from config import config
+
+class DataProcessing:
+    def __init__(self, i_data, o_data, data_num, save_dir):
+        self.data_num = data_num
+
+        # ob = config["output_bound"]
+        # i_data, o_data = self.preprocessing(i_data, o_data, ob)
+
+        sr = config["split_ratio"]
+        train_i, train_o, test_i, test_o = \
+            self.dataSplit(i_data, o_data, sr[0], sr[1])
+        
+        self.n = Normalize(train_i, train_o, save_dir)
+        self.train_input, self.train_output = \
+            self.n.dataNormalize(train_i, train_o)
+        self.test_input, self.test_output = \
+            self.n.dataNormalize(test_i, test_o)
+        
+    def dataSplit(self, i_data, o_data, tr = 0.85, te = 0.15):
+        data = np.append(i_data, o_data, axis = 1)
+
+        train_i = []
+        train_o = []
+        test_i = []
+        test_o = []
+
+        for i in range(self.data_num):
+            if i < int(self.data_num * tr):
+                train_i.append(data[i, 0:3])
+                train_o.append(data[i, 3:])
+            else:
+                test_i.append(data[i, 0:3])
+                test_o.append(data[i, 3:])
+
+        train_i = np.array(train_i)
+        train_o = np.array(train_o)
+        test_i = np.array(test_i)
+        test_o = np.array(test_o)
+
+        return train_i, train_o, test_i, test_o
+
+    def preprocessing(self, i_data, o_data, bound):
+        # sort data
+        sorted_ind = sorted(range(len(o_data)), key = lambda k:o_data[k][0])
+        sorted_i_data = np.array([i_data[i] for i in sorted_ind])
+        sorted_o_data = np.array([o_data[i] for i in sorted_ind])
+
+        # cut data
+        sorted_input = []
+        sorted_output = []
+        for i in range(len(o_data)):
+            if sorted_o_data[i] > bound[0]:
+                if sorted_o_data[i] > bound[1]:
+                    break
+                sorted_input.append(sorted_i_data[i])
+                sorted_output.append(sorted_o_data[i])
+        sorted_input = np.array(sorted_input)
+        sorted_output = np.array(sorted_output)
+        return sorted_input, sorted_output
